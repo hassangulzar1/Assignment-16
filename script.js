1f135d5842cc053dc09b6ca74a515444639e44d4
hassangulzar1 <hassangulzar121@gmail.com>
2023-07-13 13:43:33 +0500

    commit

--- script.js
+++ script.js
@@ -6,10 +6,45 @@ addUser.addEventListener("click", function () {
     inputSection.classList.remove("d-none");
     inputSection.classList.add("d-block");
   }
+  if (localStorage.length === 0) {
+    localStorage.setItem("data", JSON.stringify(personData));
+  }
 });
 
 // Generate User List Logic
-generateBtn.addEventListener("click", function () {});
+generateBtn.addEventListener("click", function () {
+  if (localStorage.length === 0) {
+    alert("Please Enter Users");
+  } else {
+    let data = JSON.parse(localStorage.getItem("data"));
+    data.forEach((e, i) => {
+      let { name, age, email, education, digit } = e;
+      let forIndex = document.createElement("h3");
+      forIndex.innerText = `${i + 1} :-`;
+      indexData.append(forIndex);
+
+      let forName = document.createElement("h3");
+      forName.innerText = `${name}`;
+      nameData.append(forName);
+
+      let forAge = document.createElement("h3");
+      forAge.innerText = `${age}`;
+      ageData.append(forAge);
+
+      let forEmail = document.createElement("h3");
+      forEmail.innerText = `${email}`;
+      emailData.append(forEmail);
+
+      let forEdu = document.createElement("h3");
+      forEdu.innerText = `${education}`;
+      eduData.append(forEdu);
+
+      let fordigit = document.createElement("h3");
+      fordigit.innerText = `${digit}`;
+      digitData.append(fordigit);
+    });
+  }
+});
 
 // Save Date from Inputs
 Save.addEventListener("click", function (e) {
@@ -36,9 +71,10 @@ Save.addEventListener("click", function (e) {
       education: education.value,
       digit: digit.value,
     };
-    // let convertedData = JSON.stringify(data);
-    personData.push(data);
-    localStorage.setItem("data", JSON.stringify(personData));
+
+    let array = JSON.parse(localStorage.getItem("data"));
+    array.push(data);
+    localStorage.setItem("data", JSON.stringify(array));
 
     let inputs = document.querySelectorAll("input");
     inputs.forEach((e) => {
@@ -51,8 +87,7 @@ Save.addEventListener("click", function (e) {
     inputSection.classList.add("d-none");
   }
 });
-let data = JSON.parse(localStorage.data);
 
-data.forEach((e, i) => {
-  let { name, age, education, digit, email } = e;
-});
+// data.forEach((e, i) => {
+//   let { name, age, education, digit, email } = e;
+// });
